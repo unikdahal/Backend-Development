333e4b422d822c6d03e6f78795db94375bf88fad
unikdahal <unikdahal03@gmail.com>
2024-01-17 17:55:26 +0530

    CORS and cookie-parser Setup Done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at PORT ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTED FAILED !!!", err);
+  });
 
 /* Code for connecting database in the same index.js file
 
