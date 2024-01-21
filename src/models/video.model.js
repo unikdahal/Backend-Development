5fa6902543b15631c86f801db1c38d81fc79ef3e
unikdahal <unikdahal03@gmail.com>
2024-01-21 20:08:53 +0530

    User and Video Data modelling

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, //Cloudinary URL
+      required: true,
+    },
+    thumbnail: {
+      type: String, // Cloudinary URL
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // through cloudinary
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
